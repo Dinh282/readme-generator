b23f48aa7eb0b3b975e9880295409b403771a932
Dinh Nguyen <nguyen_dinh282@yahoo.com>
2023-05-12 06:36:22 -0400

    add list of questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,71 @@
 // TODO: Include packages needed for this application
+import  inquirer  from "inquirer";
+import  fs  from 'fs';
+import chalk from 'chalk';
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: 'input',
+    name: 'username',
+    message: chalk.blue('What is your GitHub username?'),
+  },   
+  {
+    type: 'input',
+    name: 'email',
+    message: chalk.blue('What is your Email address?'),
+  },  
+  {
+    type: 'input',
+    name: 'title',
+    message: chalk.blue('What is the title of your project?'),
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'What is the description for this project?',
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'What are the installation instructions?',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'What is the information on usage?',
+  },
+  {
+    type: 'input',
+    name: 'contribution',
+    message: 'What are the contribution guidelnes?',
+  },
+  {
+    type: 'input',
+    name: 'test',
+    message: 'What are the test instructions?',
+  },
+
+
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+    fs.writeFile(fileName, htmlTL, (err) =>
+    err ? console.log(err) : console.log('Data logged! HTML file generated!')
+  );
+
+
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    
+    
+}
 
 // Function call to initialize app
 init();
